74965f4328cf5eabb8086599849da0fc1420a978
maanderson <maanderson@localhost>
2020-08-31 09:59:58 +0700

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

---
+++ src/Unit1_StringUserInput/MathClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Unit1_StringUserInput;
+
+/**
+ *
+ * @author maanderson
+ */
+public class MathClass {
+    public static void main(String[] args) {
+        double radius = 4;
+        double circleArea = Math.PI * Math.pow(radius, 2);
+        
+        
+    }
+}
